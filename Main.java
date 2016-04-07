edd4df27f32343856ce0227c8b80484085c8f64a
chocozz <kthanyarat.b@gmail.com>
2016-04-07 22:36:07 +0700

    #02 สร้างส่วนของยานสีเขียว .. .เพิ่มโค้ดไฟล์ Main

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
+package f2.spw;
+
 import javax.swing.*;
 import java.awt.*;
 
-
 public class Main{
 	public static void main(String args[]){
-		JFrame f = new JFrame("5410110223");
-		
-		JPanel p = new JPanel();
-		p.setBackground(Color.BLACK);
-		f.add(p);
 
+		JFrame f = new JFrame("5410110223");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(400, 650);
-		f.setVisible(true);		// show 	
+		f.setSize(400, 630);
+		f.getContentPane().setLayout(new BorderLayout());
+
+		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+		GamePanel gp = new GamePanel();
+		GameEngine engine = new GameEngine(gp, v);
+		f.getContentPane().add(gp, BorderLayout.CENTER);
+		f.setVisible(true);		// show 
+		
+		engine.start();
+			
 	}
 }
