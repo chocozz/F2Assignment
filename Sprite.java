55eb785c621a5abb0a407127e1893a396eeeb7f3
chocozz <kthanyarat.b@gmail.com>
2016-04-07 22:43:10 +0700

    #02 สร้างส่วนของยานสีเขียว .. .สร้างไฟล์ Sprite

---
+++ Sprite.java
@@ -0,0 +1,25 @@
+package f2.spw;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+
+public abstract class Sprite {
+	int x;
+	int y;
+	int width;
+	int height;
+	
+	public Sprite(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	abstract public void draw(Graphics2D g);
+	
+	public Double getRectangle() {
+		return new Rectangle2D.Double(x, y, width, height);
+	}
+}
