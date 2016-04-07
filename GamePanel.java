dbf0e763747ddf880558a5d757c5b9ada6536f82
chocozz <kthanyarat.b@gmail.com>
2016-04-07 22:45:48 +0700

    #02 สร้างส่วนของยานสีเขียว .. .สร้างไฟล์ GamePanel

---
+++ GamePanel.java
@@ -0,0 +1,37 @@
+package f2.spw;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	private BufferedImage bi;	
+	Graphics2D big;
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+
+	public GamePanel() {
+		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+		big = (Graphics2D) bi.getGraphics();
+		big.setBackground(Color.BLACK);
+	}
+
+	public void updateGameUI(){
+		big.clearRect(0, 0, 400, 600);
+				
+		for(Sprite s : sprites){
+			s.draw(big);
+		}
+		
+		repaint();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(bi, null, 0, 0);
+	}
+}
