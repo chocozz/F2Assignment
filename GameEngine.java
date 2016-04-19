3a159188d1e70d6808a40085734f43d624fd2a15
chocozz <kthanyarat.b@gmail.com>
2016-04-19 15:03:56 +0700

    #07 สร้างส่วนของ Enemy เมื่อชนกับยาน แล้วให้จบเกมส์

--- GameEngine.java
+++ GameEngine.java
@@ -63,7 +63,15 @@ public class GameEngine implements KeyListener, GameReporter{
 		}
 		gp.updateGameUI(this);
 
-		
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				die();
+				return;
+			}
+		}
 
 	}
 	
